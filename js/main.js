d34ecc50f58ec97abf7c62673b07c1e9ef48da4e
John2143658709 <Johnnyschmidt1998@gmail.com>
2014-06-05 15:33:55 -0400

    spooky commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 !function(){
-const RADIUS = 500;
-const picx = 200;
-const picy = 200;
-var isExpanded = false;
-var expandedImage = -1;
+$(document).ready(function(){
 
+	const radius = 200;
+	const picx = 75;
+	const picy = 75;
+	const numpics = 6;
+	const pipcs = Math.PI*2/numpics;
+	var expandedImage = -1;
+	var cont = $("#contain");
+	cont.css("width",radius*2+20);
+	cont.css("height",radius*2+20);
+	
+	function toggleState(num){
+		if(expandedImage>0){
+			$(".clock-imageholder").each(function(){
+				$(this)
+					.animate({left:Math.sin(pipcs)*200-picx/2,top:Math.cos(pipcs)*200-picy/2});
+			});
+		}else{
+		}
+	}
+	
+	$("<style>")
+		.prop("type", "text/css")
+		.html("\
+			.clock-imagecontain {\
+				position: absolute;\
+				width: " + picx + ";\
+				height: " + picy + ";\
+			}")
+		.appendTo("head")
+	;
+	log("wow");
+	for(var i = 0; i < numpics; i++){
+		$("<div/>",{class:"vis clock-imageholder",clockID:i, width: picx, height: picy})
+			.html("spooky"+ i)
+			.appendTo(cont)
+		;
+	}
+}); 
 }();
